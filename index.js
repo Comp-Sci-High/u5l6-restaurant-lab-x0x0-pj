b39e25e44c6a252a167e1834736f911e33643154
x0x0-pj <pryah.statom26@compscihigh.org>
2026-03-27 23:17:32 +0000

    finished

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 // Task 1: Set up server [1 pt]
 // Install + import express and mongoose 
  
-const express = require("express");
+const express = require("express")
 const mongoose = require("mongoose")
 
 const app = express()
 // Create app instance
-app.use((req, res, next) => {
-  console.log(req.method, req.url);
-  next();
-});
 
 // Add middleware to process JSON request body
 app.use(express.json())
@@ -38,28 +34,53 @@ const menuSchema = new mongoose.Schema({
   name: {type: String, required: true},
   cost: {type: Number, default: 9.99 },
   isVegan : {type: Boolean, required: true},
-  addOns: {type: String, unique: true },
+  addOns: {type: String, unique: true }
 })
 
 // Task 3: Define the model for the MenuItem [1 pt]
-const menu = mongoose.model("Menu", menuSchema, "Menus")
+const Menu = mongoose.model("Menu", menuSchema, "Menus")
 
 // Task 4: Define a POST route at /menu/test that adds a test menu item to the database [2 pt]
 // The values for the menu item should be written in the code, NOT in Postman
 // Test this route from Postman (make public!) and make sure your test item is in the DB
 
+app.post("/menu/test", async (req,res) => {
+const testMenu = await new Menu({
+name: req.body.name,
+cost: req.body.cost,
+isVegan: req.body.isVegan,
+addOns: req.body.addOns
+  }).save()
+  res.json(testMenu)
+})
+}
 
 
 // Task 5: Define a GET route at /menu that returns all menu items as a JSON [2 pt]
+app.get("/menu", async (req,res) => {
 
+  const menus = await Menu.find({})
+
+  res.json(menus)
+})
 
 
 // Task 6: Define a GET route at /menu/value that returns only menu items that cost less than 5 [2 pts]
 
-
+app.get("/menu/value", async (req,res) => {
+const menuVal = await Menu.find({cost:{$lt:5}})
+  res.json(menuVal)
+})
 
 // Task 7: Define a POST route at /menu/new that adds a new menu item [2 pts]
 // The values for the menu item should come from the request body
 // Test this route from Postman (make public!) and make sure the user's item is in the DB
-
-}
+app.post("/menu/new", async (req,res) => {
+const Umen = await new Menu({
+name: req.body.name,
+cost: req.body.cost,
+isVegan: req.body.isVegan,
+addOns: req.body.addOns
+  }).save()
+  res.json(Umen)
+})
